e8646184f048ce69c3e8730f01e869553b765664
Shuguftha99 <shuguftha14@gmail.com>
2025-12-23 21:29:02 +0530

    Update Activity10.java

--- Java/Activities/Activity10.java
+++ Java/Activities/Activity10.java
@@ -6,23 +6,23 @@ public class Activity10 {
 
 	public static void main(String[] args) {
 		HashSet<String> hs=new HashSet<String>();
-		hs.add("sravani");
-		hs.add("vallipalli");
-		hs.add("vivek");
-		hs.add("geda");
-		hs.add("syamala");
-		hs.add("vallipalli");
+		hs.add("Shiva");
+		hs.add("Ajmer");
+		hs.add("sabrina");
+		hs.add("singh");
+		hs.add("Katy");
+		hs.add("Ajmer");
 		System.out.println(hs);
 		System.out.println("size of hashset is " +hs.size());
 		
-		hs.remove("vallipalli");
+		hs.remove("Ajmer");
 		System.out.println(hs);
 		System.out.println("size of hashset is " +hs.size());
-		if(hs.contains("vallipalli")) {
-			System.out.println("vallipalli is present in set");
+		if(hs.contains("Ajmer")) {
+			System.out.println("Ajmer is present in set");
 		}
 		else {
-			System.out.println("vallipalli is not present in set");
+			System.out.println("Ajmer is not present in set");
 		}
 		System.out.println(hs);
 		System.out.println("size of hashset is " +hs.size());
@@ -30,3 +30,4 @@ public class Activity10 {
 	}
 
 }
+
